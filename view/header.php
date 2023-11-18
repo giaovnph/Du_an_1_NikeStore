c3f16bc8beb189b3d3c7131a4a4cad923691fac7
giaovnph <giaovnph38004@fpt.edu.vn>
2023-11-18 12:30:41 +0700

    fix:header

--- view/header.php
+++ view/header.php
@@ -13,28 +13,26 @@
 </head>
 
 <body>
-  <header>
+<header> <!-- Phần Menu ngang -->
     <div class="mn">
-      <div class="logo">
-        <a href="index.php"><img src="images/th.jpg" alt="" /></a>
-      <div class="logo"> <a href="index.php"><img src="images/Logo.png" alt="" /></a>
+      <div class="logo"> <a href="index.php"><img src="images/th.jpg" alt="" /></a>
 
       </div>
       <div class="menu">
         <ul>
           <li><a href="danhsach.php">Mới và nổi bật</a></li>
-          <li><a href="#">Nam</a></li>
-          <li><a href="#">Nữ</a></li>
-          <li><a href="#">Trẻ em</a></li>
+          <li><a href=" ">Nam </a></li>
+          <li><a href=" ">Nữ</a></li>
+          <li><a href=" ">trẻ em</a></li>
         </ul>
       </div>
-
-      <form method="post">
+      <form method="post" ?>
         <div class="tk">
           <div class="search-box" style="margin-left: 120px;">
-            <div style="border-right: 1px solid gray"></div>
+            <div style=" border-right: 1px solid gray">
+            </div>
             <div style="padding-left: 20px">
-              <input id="search-input" placeholder="Tìm kiếm" name="tk" type="text" />
+              <input id="search-input" placeholder="Tìm kiếm" name="tk" type="email" />
             </div>
             <div>
               <button id="search-button" type="submit">
@@ -42,7 +40,6 @@
               </button>
             </div>
           </div>
-
           <div>
             <a href="index.php?act=dangnhap">
               <img src="images/user.png" alt="" width="70px" height="45px" style="margin-left: 70px; padding-top:5px;" />
