dab830dc469949666c675de0ca42e18d941c8c58
TruongTaKhac <truongtkph40588@fpt.edu.vn>
2023-11-23 01:33:02 +0700

    feat : admin

---
+++ admin/header.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Trang Quản Trị</title>
+    <link rel="stylesheet" href="css/main.css  ">
+</head>
+<div class="boxcenter">
+        <div class="row mb headeradmin">
+        </div>
+        <div class="row mb menu">
+            <ul>
+                <li><a href="http://localhost/PHP/Du_an_1_NikeStore/">Trang chủ</a></li>
+                <li><a href="index.php?act=listdm">Danh mục</a></li>
+                <li><a href="index.php?act=listsp">Hàng hóa</a></li>
+                <li><a href="index.php?act=dskh">Khách hàng</a></li>
+                <li><a href="index.php?act=dsdh">Đơn hàng</a></li>
+                <li><a href="index.php?act=dsbl">Bình luận</a></li>
+                <li><a href="index.php?act=thongke">Thống kê</a></li>
+            </ul>
+        </div>
