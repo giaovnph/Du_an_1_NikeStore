7fc6079ced3096a59867587b9bf56df6da0d7c26
giaovnph <giaovnph38004@fpt.edu.vn>
2023-11-25 12:29:01 +0700

    fix:css

--- admin/header.php
+++ admin/header.php
@@ -5,19 +5,23 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Trang Quản Trị</title>
-    <link rel="stylesheet" href="css/main.css  ">
+    <link rel="stylesheet" href="css/style.css  ">
 </head>
-<div class="boxcenter">
-        <div class="row mb headeradmin">
-        </div>
-        <div class="row mb menu">
-            <ul>
-                <li><a href="http://localhost/PHP/Du_an_1_NikeStore/">Trang chủ</a></li>
+    <div class="mn">
+        <div class="logo"> 
+            <h2>ADMIN</h2>
+            <a href="index.php"><img src="../images/Logo.png" alt="" /></a>
+        </div> 
+      <div class="menu">
+        <ul>
+                <li><a href="http://localhost/Du_an_1_NikeStore/admin/">Trang chủ</a></li>
                 <li><a href="index.php?act=listdm">Danh mục</a></li>
                 <li><a href="index.php?act=listsp">Hàng hóa</a></li>
                 <li><a href="index.php?act=dskh">Khách hàng</a></li>
                 <li><a href="index.php?act=dsdh">Đơn hàng</a></li>
                 <li><a href="index.php?act=dsbl">Bình luận</a></li>
                 <li><a href="index.php?act=thongke">Thống kê</a></li>
-            </ul>
-        </div>
+        </ul>
+      </div>
+      
+    </div>
--- model/taikhoan.php
+++ model/taikhoan.php
@@ -6,12 +6,12 @@
         }
 
         function insert_taikhoan($email, $user, $pass, $sdt){
-            $sql="insert into taikhoan(email, username, password, sdt) values('$email','$user','$pass','$sdt')";
+            $sql="insert into taikhoan(email, user, pass, sdt) values('$email','$user','$pass','$sdt')";
             pdo_execute($sql);
         }
 
         function kttaikhoan($user, $pass){
-            $sql="select * from taikhoan where username='".$user."' AND password='".$pass."'";
+            $sql="select * from taikhoan where user='".$user."' AND pass='".$pass."'";
             $tk=pdo_query_one($sql);
             return $tk;
         }
@@ -23,7 +23,7 @@
         }
 
         function update_taikhoan($id, $email, $user, $pass, $diachi, $sdt){
-            $sql="update taikhoan set email='".$email."', username='".$user."', password='".$pass."', diachi='".$diachi."', sdt='".$sdt."' where id =".$id;
+            $sql="update taikhoan set email='".$email."', user='".$user."', pass='".$pass."', diachi='".$diachi."', sdt='".$sdt."' where id =".$id;
             pdo_execute($sql);
         }
 ?>
