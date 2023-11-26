ad92cac67cc489be4f200aa8f4398a93682ba508
giaovnph <giaovnph38004@fpt.edu.vn>
2023-11-26 22:47:55 +0700

    fix:giohang

---
+++ thanhtoan.php
@@ -0,0 +1,110 @@
+<?php
+
+?>
+<link rel="stylesheet" href="css.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css ">
+
+
+
+<div class="allgh">
+    <div class="tuido">
+        <img src="images/th.jpg" alt="">
+        <div>
+            <span>Túi đồ của bạn</span>
+        </div>
+        <hr>
+    </div>
+    <div class="h1">
+        <span>Giỏ hàng <i style="font-size: 18px;" class="fa-solid fa-cart-shopping"></i></span>
+    </div>
+
+
+    <div class="giohangtt">
+
+        <div class="anhsp">
+            <img src="Anh_Giay/a1.jpg">
+            <hr>
+        </div>
+
+        <div class="chitiet">
+            <p class="ten">Jordan 4 mid red Gym</p>
+
+            <div class="gioithieu">Giày nam/nữ</div>
+            <div class="gioithieu">Cánh buồm/Đỏ đô/Trắng/Đen</div>
+            <label class="kichco">Kích cỡ</label>
+            <select name="kichco" id="kichco">
+                <option value="39">39</option>
+                <option value="">40</option>
+                <option value="">41</option>
+                <option value="">42</option>
+                <option value="">43</option>
+                <option value="">44</option>
+                <option value="">45</option>
+            </select>
+
+            <label class="soluong">Số lượng</label>
+            <select name="soluong" id="soluong">
+                <option value="">1</option>
+                <option value="">2</option>
+                <option value="">3</option>
+                <option value="">4</option>
+                <option value="">5</option>
+                <option value="">6</option>
+                <option value="">7</option>
+                <option value="">8</option>
+                <option value="">9</option>
+                <option value="">10</option>
+
+            </select>
+            <div class="icontt">
+                <a href=""><i id="i1" class="fa-regular fa-heart"></i></a>
+
+                <a href=""><i id="i2" class="fa-regular fa-trash-can"></i></a>
+            </div>
+        </div>
+
+
+
+
+        <div class="ten tien">
+            <span>2.500.000 <span class="vnd">vnđ</span></span>
+        </div>
+
+        <div class="thanhtoan">
+            <span class="tieude">Bảng tóm tắt thanh toán</span>
+
+
+            <div class="tien">
+                <span>Chọn kích cỡ</span>
+
+            </div>
+            <div class="tien">
+                <span>Chọn kích cỡ</span>
+
+            </div>
+            <hr>
+
+            <div class="tien">
+                <span>Tổng cộng<span class="tien1">4.500.000 <span class="vnd">vnđ</span></span></span>
+
+            </div>
+            <hr>
+
+            <div class="nuttt">
+                <a href=""> <button>Thanh toán</button></a>
+            </div>
+
+        </div>
+
+
+    </div>
+
+
+
+    <div class="goiy">
+        <span>Sản phẩm bạn cũng có thể yêu thích</span>
+    </div>
+</div>
+<?php
+include "./view/footer.php";
+?>
