16a122137c853ed8e545b8e51f7710e93f5e106b
giaovnph <giaovnph38004@fpt.edu.vn>
2023-12-01 04:15:50 +0700

    fix:css

--- view/header.php
+++ view/header.php
@@ -9,7 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css ">
     <link rel="stylesheet" href="view/css/cart.css">
     <link rel="stylesheet" href="view/css/bill.css">
-    <title>Document</title>
+    <title></title>
 </head>
 
 <body class="nikestore">
