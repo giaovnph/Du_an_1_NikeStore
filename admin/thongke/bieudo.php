b9435ae14212547f3d46f97a699ad2202047cd8e
chienbui004 <chienvlog2k4@gmail.com>
2023-12-07 13:16:33 +0700

    fix:bieudo

--- admin/thongke/bieudo.php
+++ admin/thongke/bieudo.php
@@ -1,29 +1,53 @@
 <div id="piechart"></div>
 
-<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 
-<script type="text/javascript">
-// Load google charts
-google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+<canvas id="myMixedChart" width="400" height="400"></canvas>
 
-// Draw the chart and set the chart values
-function drawChart() {
-  var data = google.visualization.arrayToDataTable([
-  ['Danh mục', 'Số lượng sản phẩm'],
-  <?php
-        foreach($dstk as $tk){
-            extract($tk);
-            echo "['".$tk['name']."',".$tk['countsp']."],";
-        } 
-    ?>
-]);
+<script>
+    // Lấy tham chiếu đến thẻ canvas
+    var ctx = document.getElementById('myMixedChart').getContext('2d');
 
-  // Optional; add a title and set the width and height of the chart
-  var options = {'title':'DANH MỤC SẢN PHẨM KINH DOANH', 'width':550, 'height':400};
+    // Dữ liệu mẫu cho biểu đồ mixed chart
+    var data = {
+        labels: ['Mới và nổi bật', 'Trẻ em', 'Nữ', 'Nam'],
+        datasets: [{
+            label: 'Monthly Sales (Line)',
+            type: 'line',
+            data: [20, 60, 70, 65],
+            borderColor: 'rgb(75, 192, 192)',
+            fill: false
+        }, {
+            label: 'Monthly Revenue (Bar)',
+            type: 'bar',
+            data: [200, 180, 220, 210],
+            backgroundColor: 'rgba(100, 10, 0.1, 0.05)'
+        }]
+    };
 
-  // Display the chart inside the <div> element with id="piechart"
-  var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-  chart.draw(data, options);
-}
+    // Cấu hình các tùy chọn cho biểu đồ
+    var options = {
+        responsive: true,
+        maintainAspectRatio: false,
+        title: {
+            display: true,
+            text: 'Biểu đồ Mixed Chart đơn giản'
+        },
+        scales: {
+            x: {
+                type: 'category',
+                labels: data.labels
+            },
+            y: {
+                beginAtZero: true
+            }
+        }
+    };
+
+    // Tạo biểu đồ mixed chart
+    var myMixedChart = new Chart(ctx, {
+        type: 'bar',
+        data: data,
+        options: options
+    });
 </script>
