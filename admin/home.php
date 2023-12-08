4c46bb4ee64335e53451f14f8137343544f76a69
chienbui004 <chienvlog2k4@gmail.com>
2023-12-08 15:27:17 +0700

    fix:

--- admin/home.php
+++ admin/home.php
@@ -1,96 +1,129 @@
-<div class="row">
-    <div class="" style="text-align: center;">
-        <h2>DANH SÁCH LOẠI HÀNG</h2>
+<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+<style>
+        .containerr {
+        display: grid;
+        grid-template-columns: repeat(auto-fill, minmax(48%, 1fr));
+        margin-left: 130px;
+        }
+
+        .col-md-6 {
+            width: 100%;
+            margin-left: 100px;
+        }
+
+        .col-md,
+        .col-md1 {
+            margin: 0;
+            text-align: center;
+        }
+
+        .frmcontent {
+            margin-right: 160px;
+        }
+
+        h2,
+        label,
+        span {
+            color: black;
+        }
+    </style>
+<div class="containerr">
+        <!-- Biểu đồ Loại Hàng -->
+        <div class="col-md-6">
+            <div class="col-md">
+                <div class="" style="text-align: center;">
+                    <h2>DANH SÁCH LOẠI HÀNG</h2>
+                </div>
+                <div class="frmcontent w60">
+                    <canvas id="myChart"></canvas>
+                </div>
+
+                <script>
+                    var listdanhmuc = <?php echo json_encode($listdanhmuc); ?>;
+
+                    var labels = [];
+                    var data = [];
+
+                    listdanhmuc.forEach(function(danhmuc) {
+                        labels.push(danhmuc.name);
+                        data.push(danhmuc.id);
+                    });
+
+                    var ctx = document.getElementById('myChart').getContext('2d');
+                    var myChart = new Chart(ctx, {
+                        type: 'bar',
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: 'MÃ LOẠI',
+                                data: data,
+                                backgroundColor: 'rgba(90, 192, 192, 1)',
+                                borderColor: 'black',
+                                borderWidth: 1
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                </script>
+            </div>
+        </div>
+
+        <!-- Biểu đồ Thống kê -->
+        <div class="col-md-6">
+            <div class="col-md1">
+                <div class="mb" style="text-align: center;">
+                    <h2>THỐNG KÊ</h2>
+                </div>
+                <div class="frmcontent w60">
+                    <canvas id="myLineChart"></canvas>
+                </div>
+
+                <script>
+                    var dstk = <?php echo json_encode($dstk); ?>;
+                    var labels = [];
+                    var countspData = [];
+
+                    dstk.forEach(function(tk) {
+                        labels.push(tk.name);
+                        countspData.push(tk.countsp);
+                    });
+
+                    var ctx = document.getElementById('myLineChart').getContext('2d');
+                    var myLineChart = new Chart(ctx, {
+                        type: 'line',
+                        data: {
+                            labels: labels,
+                            datasets: [{
+                                label: 'Số lượng',
+                                data: countspData,
+                                borderColor: 'rgba(255, 0, 0, 1)',
+                                borderWidth: 2,
+                                fill: false
+                            }]
+                        },
+                        options: {
+                            scales: {
+                                y: {
+                                    beginAtZero: true
+                                }
+                            }
+                        }
+                    });
+                </script>
+            </div>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
-    <div class="frmcontent w60">
-        <canvas id="myChart"></canvas>
-    </div>
-
-    <script>
-        var listdanhmuc = <?php echo json_encode($listdanhmuc); ?>;
-
-        var labels = [];
-        var data = [];
-
-        listdanhmuc.forEach(function(danhmuc) {
-            labels.push(danhmuc.name);
-            data.push(danhmuc.id);
-        });
-
-        var ctx = document.getElementById('myChart').getContext('2d');
-        var myChart = new Chart(ctx, {
-            type: 'bar',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'MÃ LOẠI',
-                    data: data,
-                    backgroundColor: 'white',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 1
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
-</div>
-
-<!-- thong ke -->
-<div class="row">
-    <div class=" mb" style="text-align: center;">
-        <h2>THỐNG KÊ</h2>
-    </div>
-    <div class="frmcontent w60">
-        <canvas id="myLineChart"></canvas>
-    </div>
-
-    <script>
-
-        var dstk = <?php echo json_encode($dstk); ?>;
-        var labels = [];
-        var countspData = [];
-
-        dstk.forEach(function(tk) {
-            labels.push(tk.name);
-            countspData.push(tk.countsp);
-        });
-
-        var ctx = document.getElementById('myLineChart').getContext('2d');
-        var myLineChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: labels,
-                datasets: [{
-                    label: 'Số lượng',
-                    data: countspData,
-                    backgroundColor: 'white',
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 2,
-                    fill: false
-                }]
-            },
-            options: {
-                scales: {
-                    y: {
-                        beginAtZero: true
-                    }
-                }
-            }
-        });
-    </script>
-</div>
 
 
 <!-- DANH SACH TAI KHOAN -->
 <div class="row">
-    <div class="mb" style="text-align: center;">
+    <div class="" style="text-align: center;">
         <h2>DANH SÁCH TÀI KHOẢN</h2>
     </div>
     <div class=" frmcontent">
--- admin/thongke/bieudo.php
+++ admin/thongke/bieudo.php
@@ -8,28 +8,56 @@
         <canvas id="myLineChart"></canvas>
     </div>
 
+    <div class="frmcontent w60">
+        <canvas id="myMixedChart"></canvas>
+    </div>
+
     <script>
+        // Dữ liệu từ PHP
         var dstk = <?php echo json_encode($dstk); ?>;
+
+        // Tạo mảng cho biểu đồ
         var labels = [];
-        var countspData = [];
+        var maxgiaData = [];
+        var mingiaData = [];
+        var avgData = [];
 
         dstk.forEach(function(tk) {
             labels.push(tk.name);
-            countspData.push(tk.countsp);
+            maxgiaData.push(tk.maxgia);
+            mingiaData.push(tk.mingia);
+            avgData.push(tk.avg);
         });
 
-        var ctx = document.getElementById('myLineChart').getContext('2d');
-        var myLineChart = new Chart(ctx, {
-            type: 'line',
+        // Tạo biểu đồ mixed
+        var ctx = document.getElementById('myMixedChart').getContext('2d');
+        var myMixedChart = new Chart(ctx, {
+            type: 'bar',
             data: {
                 labels: labels,
-                datasets: [{
-                    label: 'Số lượng',
-                    data: countspData,
-                    borderColor: 'rgba(75, 192, 192, 1)',
-                    borderWidth: 2,
-                    fill: false
-                }]
+                datasets: [
+                    {
+                        label: 'Giá cao nhất',
+                        data: maxgiaData,
+                        backgroundColor: 'rgba(90, 192, 192, 1)',
+                        borderColor: 'black',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Giá thấp nhất',
+                        data: mingiaData,
+                        backgroundColor: 'rgba(255, 0, 0, 1)',
+                        borderColor: 'black',
+                        borderWidth: 1
+                    },
+                    {
+                        label: 'Giá trung bình',
+                        data: avgData,
+                        backgroundColor: '#FFD700',
+                        borderColor: 'black',
+                        borderWidth: 1
+                    }
+                ]
             },
             options: {
                 scales: {
