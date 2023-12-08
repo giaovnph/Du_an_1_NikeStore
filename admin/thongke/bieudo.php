a18a6fc903dd7d775ac01536d53944ec222e5a9c
chienbui004 <chienvlog2k4@gmail.com>
2023-12-08 16:04:48 +0700

    fix:

--- admin/thongke/bieudo.php
+++ admin/thongke/bieudo.php
@@ -39,22 +39,22 @@
                     {
                         label: 'Giá cao nhất',
                         data: maxgiaData,
-                        backgroundColor: 'rgba(90, 192, 192, 1)',
-                        borderColor: 'black',
+                        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+                        borderColor: 'rgba(255, 99, 132, 1)',
                         borderWidth: 1
                     },
                     {
                         label: 'Giá thấp nhất',
                         data: mingiaData,
-                        backgroundColor: 'rgba(255, 0, 0, 1)',
-                        borderColor: 'black',
+                        backgroundColor: 'rgba(54, 162, 235, 0.2)',
+                        borderColor: 'rgba(54, 162, 235, 1)',
                         borderWidth: 1
                     },
                     {
                         label: 'Giá trung bình',
                         data: avgData,
-                        backgroundColor: '#FFD700',
-                        borderColor: 'black',
+                        backgroundColor: 'rgba(255, 206, 86, 0.2)',
+                        borderColor: 'rgba(255, 206, 86, 1)',
                         borderWidth: 1
                     }
                 ]
